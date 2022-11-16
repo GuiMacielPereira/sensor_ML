d99ea13e76ac372e217bf83a1a5ff9c2f4be3b15
Gui Pereira <gjmacielpereira1@sheffield.ac.uk>
2022-11-16 15:32:49 +0000

    Improved the architecture to use 2 convolutional layers. Working and achieving very high accuracies.

--- data_processing/toy_pytorch.sync.py
+++ data_processing/toy_pytorch.sync.py
@@ -49,7 +49,7 @@ print("Size of train set:", Xtrain.shape)
 # Function to print accuracy of validation set while training
 def checkAcc(x, y, model, set="Validation"):
     scores = model(x)
-    _, preds = scores.max(1)
+    _, preds = scores.max(1)    # Returns indices alongside axis=1
     num_correct = (preds == y).sum()
     num_samples = preds.size(0)
     acc = float(num_correct) / num_samples
@@ -75,8 +75,7 @@ yv = torch.tensor(yval, device=device, dtype=torch.long)
 #%%
 # Define a general procedure to train a model
 
-def trainModel(model, loss_fn, optimizer, x, y, xv, yv, N=2000):
-# learning_rate = 1e-4
+def trainModel(model, loss_fn, optimizer, x, y, xv, yv, N=1000):
     for t in range(N):
 
         y_pred = model(x)
@@ -112,80 +111,95 @@ loss_fn = torch.nn.CrossEntropyLoss()
 
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 
-trainModel(model, loss_fn, optimizer, x, y, xv, yv)
+# trainModel(model, loss_fn, optimizer, x, y, xv, yv)
 
 #%%
 # Test model on test data
 xt= torch.tensor(Xtest, device=device, dtype=dtype)
 yt= torch.tensor(ytest, device=device, dtype=torch.long)
-with torch.no_grad():
-    checkAcc(xt, yt, model, set="Test")
+# with torch.no_grad():
+#     checkAcc(xt, yt, model, set="Test")
 
 #%%
 # Normalizing the data and building a deeper layer model
 def normalize(x, xtr):
     # Use quantities from training data set
-    avgmax = np.mean(np.max(xtr, axis=1))
+    # avgmax = np.mean(np.max(xtr, axis=1))
+    avgmax = np.max(x, axis=1)[:, np.newaxis]
     return x / avgmax 
 
 xdata = [Xfold, Xval, Xtest]
 for i in range(len(xdata)):
-    xdata[i] = normalize(xdata[i], Xfold)
+    xdata[i] = normalize(xdata[i], Xfold)[:, np.newaxis, :]
 
 # Pass onto pytorch tensors
 for i in range(len(xdata)):
     xdata[i] = torch.tensor(xdata[i], device=device, dtype=dtype)
 
 xnorm, xvnorm, xtnorm = xdata
+print(xnorm.shape)
 
 class deepModel(torch.nn.Module):
     def __init__(self):
         super(deepModel, self).__init__()
 
+        D1 = 12 
+        D2 = 12 
+
         self.layer1 = torch.nn.Sequential(
-                torch.nn.Conv1d(1, 10, kernel_size=3),
+                torch.nn.Conv1d(1, D1, kernel_size=3, padding=1),
                 torch.nn.ReLU(),
                 torch.nn.MaxPool1d(kernel_size=2)
                 )
-        self.fc1 = torch.nn.Linear(15 * 10, 1000)
-        self.fc2 = torch.nn.Linear(1000, 2)
+        
+        self.layer2 = torch.nn.Sequential(
+                torch.nn.Conv1d(D1, D2, kernel_size=3, padding=1),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool1d(kernel_size=3)
+                )
+        # self.drop_out = torch.nn.Dropout()
+        self.fc1 = torch.nn.Linear(D2 * 5, 100)    
+        self.fc2 = torch.nn.Linear(100, 2)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
+        out = out.view(out.shape[0], -1)   # Flatten tensor
+        # out = self.drop_out(out)
         out = self.fc1(out)
         out = self.fc2(out)
         return out
 
 
-model_deep = deepModel()
+model_deep = deepModel().to(device)
 loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+optim_deep = torch.optim.SGD(model_deep.parameters(), lr=1e-3, momentum=0.9)
 
-print("Same model but with normalized data:")
-trainModel(model_deep, loss_fn, optimizer, xnorm, y, xvnorm, yv)
+trainModel(model_deep, loss_fn, optim_deep, xnorm, y, xvnorm, yv, 10000)
 with torch.no_grad():
-    checkAcc(xtnorm, yt, model, set="Test")
+    checkAcc(xtnorm, yt, model_deep, set="Test")
 
 
 #%%
 import matplotlib.pyplot as plt
+
+def plotTest(Xtest, ytest, predictions):
+    usr1 = predictions==0
+    usr2 = ~usr1
+    for j, usr in enumerate([usr1, usr2]):
+        X1 = Xtest[usr]
+        labels = ytest[usr]
+        plt.figure(figsize=(15, 5))
+        plt.suptitle(f"Predictions for user {j}")
+        for i, (x, lab) in enumerate(zip(X1, labels)):
+            plt.subplot(1, len(X1), i+1)
+            plt.plot(range(len(x)), x, "b.")
+            plt.title(f"Real User: {lab}")
+
 # Plot results
 with torch.no_grad():     # Very important to specify no_grad to avoid automatic differentiation of this step
-    scores = model(xt)
+    scores = model_deep(xtnorm).view(xtnorm.shape[0], -1)
     _, preds = scores.max(1)
     predictions = preds.detach().numpy()
 
-usr1 = predictions==0
-usr2 = ~usr1
-
-for j, usr in enumerate([usr1, usr2]):
-    X1 = Xtest[usr]
-    labels = ytest[usr]
-    plt.figure(figsize=(20, 3))
-    plt.suptitle(f"Predictions for user {j}")
-    for i, (x, lab) in enumerate(zip(X1, labels)):
-        plt.subplot(1, len(X1), i+1)
-        plt.plot(range(len(x)), x, "b.")
-        plt.title(f"Real User: {lab}")
-
+plotTest(Xtest, ytest, predictions)
