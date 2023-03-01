9468afd472d0a4d3d66a9e49c4b83ddf22e88fc7
Gui Pereira <gjmacielpereira1@sheffield.ac.uk>
2023-03-01 16:28:21 +0000

    Modified data preparation script to visualise faulty and noisy triggers that should not be included in the training data. Currently still changing this file and figuring out what the best rule for discarding faulty triggers is.

--- data_processing/data_prep.sync.py
+++ data_processing/data_prep.sync.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Width of triggers 
-width = 32
+width = 64 
 fileName = "second_collection"
 data = np.load((fileName+".npz"))
 
@@ -26,16 +26,29 @@ def findTrigIdxs(signal):
 def separateIntoTriggers(signal, trigIdx, releaseIdx, width=width):
     triggers = []
     releases = []
+
+    short_trigs= []
+    false_trigs= []
+
     for i, j in zip(trigIdx, releaseIdx):
         upSig = signal[i:i+width]
         downSig = signal[j-width:j]
 
         if np.any(upSig>=2):          # Hardcoded threshold to select only clean signals
-            # TODO: Need to confirm that approach below is matching up triggers to releases correctly
-            triggers.append(upSig) 
-            releases.append(downSig)
 
-    return np.array(triggers), np.array(releases) 
+            if np.any(upSig[-10:]<=0.5):    # Check that this length doe not include release
+                short_trigs.append(upSig)
+            else:
+                # TODO: Need to confirm that approach below is matching up triggers to releases correctly
+                triggers.append(upSig) 
+                releases.append(downSig)
+        else:
+            false_trigs.append(upSig)
+
+    print("\nNumber of signals excluded because false noise: ", len(false_trigs))
+    print("\nNumber of signals excluded becasue too short: ", len(short_trigs))
+
+    return np.array(triggers), np.array(releases), (np.array(false_trigs), np.array(short_trigs))
 
 
 def plotTriggers(triggers, nx_plots=10, ny_plots=2):
@@ -56,12 +69,25 @@ def plotSignal(signal, upTo):
 
 # %%
 # Look at a few triggers of one of the users
-key = "A"
+key = "J"
 signal = data[key]   # Only a few presses 
 print("Signal shape:", signal.shape)
 plotSignal(signal, upTo=10000)
 trigIdx, relIdx = findTrigIdxs(signal)
-triggers, releases = separateIntoTriggers(signal, trigIdx, relIdx)
+triggers, releases, faulty = separateIntoTriggers(signal, trigIdx, relIdx)
+
+# %%
+# Plot noisy triggers to make sure they are faulty signals 
+# Best to see on a matplotlib plot rather than a jupyter notebook
+false_trigs, short_trigs = faulty
+for f_trig in (false_trigs, short_trigs):
+    false_sig = np.concatenate(f_trig)
+    plt.figure()
+    plt.plot(np.arange(len(false_sig)), false_sig, "b.")
+    plt.show()
+
+exit()
+#%%
 print("triggers shape:", triggers.shape)
 print("releases shape: ", releases.shape)
 plotTriggers(triggers) 
@@ -73,7 +99,7 @@ plt.figure(figsize=(13, 7))
 n_users = len(data)
 for i, key in enumerate(data):
     trigIdx, relIdx = findTrigIdxs(data[key])
-    triggers, releases = separateIntoTriggers(data[key], trigIdx, relIdx)
+    triggers, releases, _ = separateIntoTriggers(data[key], trigIdx, relIdx)
     for j, sig in enumerate([triggers, releases]):
         plt.subplot(n_users, 2, 2*i+j+1)
         sigAvg = np.mean(sig, axis=0)
@@ -89,7 +115,7 @@ filteredData = {}
 for key in data:
     signal = data[key].flatten()
     trigIdx, relIdx = findTrigIdxs(signal)
-    triggers, releases = separateIntoTriggers(signal, trigIdx, relIdx)
+    triggers, releases, _ = separateIntoTriggers(signal, trigIdx, relIdx)
     filteredData[key+"_triggers"] = triggers
     filteredData[key+"_releases"] = releases 
     print(f"saving {key}_triggers: ", triggers.shape)
